136b1d3b575138cec48328da3eee76495ebc0a99
damirdusembaev0-max <damirdusembaev0@gmail.com>
2026-09-09 14:09:22 +0500

    Update task1.py

---
+++ lab2/task1.py
@@ -0,0 +1,12 @@
+a = float(input("Введите первое число: "))
+b = float(input("Введите второе число: "))
+
+summa = a + b
+raznost = a - b
+proizvedenie = a * b
+chastnoe = a / b
+
+print("Сумма:", summa)
+print("Разность:", raznost)
+print("Произведение:", proizvedenie)
+print("Частное:", chastnoe)
